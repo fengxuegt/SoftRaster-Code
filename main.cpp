bc2a0ff90800dbc262eb628c6a72f44f803b2354
fengxuegt <fengxuegt@163.com>
2026-08-04 17:22:11 +0800

    vao and vbo

--- main.cpp
+++ main.cpp
@@ -7,100 +7,67 @@
 #include "math/matrix.h"
 
 #pragma comment(linker, "/subsystem:console /entry:wWinMainCRTStartup" )
-Image *imagee;
-Image* texture;
-Point p1;
-Point p2;
-Point p3;
 
-math::vec4f pos1;
-math::vec4f pos2;
-math::vec4f pos3;
+//三个属性对应vbo
+uint32_t positionVbo = 0;
+uint32_t colorVbo = 0;
+uint32_t uvVbo = 0;
 
-math::mat4f modelMatrix;
-math::mat4f viewMatrix;
-math::mat4f perspectiveMatrix;
-math::mat4f screenMatrix;
+//三角形的indices
+uint32_t ebo = 0;
 
-float angle = 0.0f;
-float cameraPos = 5.0f;
-// Point p1{ 0, 0, RGBA(255, 0, 0, 255)};
-// Point p2{ 400, 900, RGBA(0, 255, 0, 255) };
-// Point p3{ 800, 0, RGBA(0, 0, 255, 255) };
-// void Prepare() {
-// 	imagee = Image::CreateImage("assets/textures/goku.jpg");
-// 	MALEOON->SetTexture(imagee);
-// 	p1.uv = math::vec2f(0.0f, 0.0f);
-// 	p2.uv = math::vec2f(0.5f, 1.0f);
-// 	p3.uv = math::vec2f(1.0f, 0.0f);
-// }
-// void transform() {
-// 	angle += 0.03f;
-// 	cameraPos += 0.01f;
-//
-// 	//模型变换
-// 	modelMatrix = math::rotate(math::mat4f(1.0f), angle, math::vec3f{ 0.0f, 1.0f, 0.0f });
-//
-// 	//模拟摄像机往后退
-// 	auto cameraModelMatrix = math::translate(math::mat4f(1.0f), math::vec3f{ 0.0f, 0.0f, cameraPos });
-// 	viewMatrix = math::inverse(cameraModelMatrix);
-//
-// 	//mvp矩阵相乘
-// 	auto sp1 = perspectiveMatrix * viewMatrix * modelMatrix * pos1;
-// 	auto sp2 = perspectiveMatrix * viewMatrix * modelMatrix * pos2;
-// 	auto sp3 = perspectiveMatrix * viewMatrix * modelMatrix * pos3;
-//
-// 	//透视除法（此处故意设计z！=0）
-// 	sp1 /= sp1.w;
-// 	sp2 /= sp2.w;
-// 	sp3 /= sp3.w;
-//
-// 	//屏幕空间
-// 	sp1 = screenMatrix * sp1;
-// 	sp2 = screenMatrix * sp2;
-// 	sp3 = screenMatrix * sp3;
-//
-// 	p1.x = sp1.x;
-// 	p1.y = sp1.y;
-//
-// 	p2.x = sp2.x;
-// 	p2.y = sp2.y;
-//
-// 	p3.x = sp3.x;
-// 	p3.y = sp3.y;
-// }
-// void Prepare() {
-// 	texture = Image::CreateImage("assets/textures/goku.jpg");
-// 	MALEOON->SetTexture(texture);
-// 	p1.color = RGBA(255, 0, 0, 255);
-// 	p1.uv = math::vec2f(0.0f, 0.0f);
-//
-// 	p2.color = RGBA(0, 255, 0, 255);
-// 	p2.uv = math::vec2f(1.0f, 1.0f);
-//
-// 	p3.color = RGBA(0, 0, 255, 255);
-// 	p3.uv = math::vec2f(1.0f, 0.0f);
-//
-// 	pos1 = {-1.5f, 0.0f, 0.0f, 1.0f};
-// 	pos2 = {1.5f, 0.0f, 0.0f, 1.0f };
-// 	pos3 = {0.0f, 2.0f, 0.0f, 1.0f };
-//
-// 	perspectiveMatrix = math::perspective(60.0f, (float)app->GetWidth() / (float)app->GetHeight(), 0.1f, 100.0f);
-// 	screenMatrix = math::screenMatrix<float>(app->GetWidth(), app->GetHeight());
-// }
-uint32_t vbo{0};
-uint32_t vao{0};
+//本三角形专属vao
+uint32_t vao = 0;
 void Prepare() {
-	vbo = MALEOON->GenBuffer();
-	MALEOON->DeleteBuffer(vbo);
+	float positions[] = {
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+	};
+
+	float colors[] = {
+		1.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f, 1.0f,
+	};
+
+	float uvs[] = {
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 0.0f,
+	};
+	uint32_t indices[] = { 0, 1, 2 };
+
+	ebo = MALEOON->GenBuffer();
+	MALEOON->BindBuffer(ELEMENT_ARRAY_BUFFER, ebo);
+	MALEOON->BufferData(ELEMENT_ARRAY_BUFFER, sizeof(indices), indices);
+	MALEOON->BindBuffer(ELEMENT_ARRAY_BUFFER, 0);
+
 	vao = MALEOON->GenVertexArray();
-	MALEOON->DeleteVertexArray(vao);
+	MALEOON->BindVertexArray(vao);
+	auto positionVBO = MALEOON->GenBuffer();
+	MALEOON->BindBuffer(ARRAY_BUFFER, positionVBO);
+	MALEOON->BufferData(ARRAY_BUFFER, sizeof(positions), positions);
+	MALEOON->VertexAttributePointer(0, 3, 3 * sizeof(float), 0);
+
+	auto colorVBO = MALEOON->GenBuffer();
+	MALEOON->BindBuffer(ARRAY_BUFFER, colorVBO);
+	MALEOON->BufferData(ARRAY_BUFFER, sizeof(colors), colors);
+	MALEOON->VertexAttributePointer(1, 4, 4 * sizeof(float), 0);
+
+	auto uvVBO = MALEOON->GenBuffer();
+	MALEOON->BindBuffer(ARRAY_BUFFER, uvVBO);
+	MALEOON->BufferData(ARRAY_BUFFER, sizeof(uvs), uvs);
+	MALEOON->VertexAttributePointer(2, 2, 2 * sizeof(float), 0);
+
+	MALEOON->BindBuffer(ARRAY_BUFFER, 0);
+	MALEOON->BindVertexArray(0);
+
+	MALEOON->PrintVao(vao);
 
 }
 void Render() {
-	// transform();
 	MALEOON->ClearSurfaceView();
-	MALEOON->DrawTriangle(p1, p2, p3);
 }
 
 
